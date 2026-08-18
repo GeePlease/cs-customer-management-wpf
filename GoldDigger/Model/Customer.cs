f785bdfe269c87c70049180130d1670bdf76d3f3
Steffi Gmeiner <053818@edu.szf.at>
2026-08-18 19:26:20 +0200

    1st commit: inital project structure

---
+++ GoldDigger/Model/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDigger.Model
+{
+    internal class Customer
+    {
+    }
+}
---
+++ GoldDigger/ViewModel/Login.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDigger.ViewModel
+{
+    internal class Login
+    {
+    }
+}
