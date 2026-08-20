4814c25ad361aa5d3df24ff318f077c555780207
Steffi Gmeiner <053818@edu.szf.at>
2026-08-20 10:44:32 +0200

    implement exit program button functionality

--- GoldDigger/ViewModel/MainWindow.cs
+++ GoldDigger/ViewModel/MainWindow.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace GoldDigger.ViewModel
 {
-    public class MainWindow
+
+    public partial class MainWindow: ObservableObject
     {
+
+        // METHODS
+
+        // Relay Command: Button "Beenden"
+        [RelayCommand]
+        private void ExitApplication()
+        {
+            Application.Current.Shutdown();
+        }
+
+
+
+    // END CLASS
     }
 }
